02528824b3da26ef95123cbc22890e7549ffa399
Pedro Alcoba <pedralcg@gmail.com>
2025-05-12 23:14:50 +0200

    Part01a - Ejer 1.4 Courseinfo

--- part01/courseinfo/src/App.jsx
+++ part01/courseinfo/src/App.jsx
@@ -1,3 +1,5 @@
+// Componente Header: Muestra el nombre del curso.
+// Recibe el nombre del curso como una prop llamada 'course'.
 const Header = (props) => {
   //console.log(props); // tip: usa console.log(props) para ver qué props recibe un componente
   return (
@@ -7,7 +9,8 @@ const Header = (props) => {
   )
 }
 
-// Define este nuevo componente en tu archivo App.jsx
+// Componente Part: Muestra el nombre de una parte y su número de ejercicios.
+// Recibe el nombre de la parte como prop 'name' y los ejercicios como prop 'exercises'.
 const Part = (props) => {
   // console.log('Props recibidas en Part:', props);
   return (
@@ -22,63 +25,64 @@ const Part = (props) => {
 };
 
 
+// Componente Content: Renderiza los componentes Part para cada sección del curso.
+// Refactorizado: Ahora recibe el array completo 'parts' como una prop.
 const Content = (props) => {
   // console.log('Props recibidas en Content:', props);
   return (
     <div>
-      {/* Pasa las propiedades 'name' y 'exercises' del objeto part1 al componente Part */}
-      <Part name={props.part1.name} exercises={props.part1.exercises} />
-      {/* Pasa las propiedades 'name' y 'exercises' del objeto part2 al componente Part */}
-      <Part name={props.part2.name} exercises={props.part2.exercises} />
-      {/* Pasa las propiedades 'name' y 'exercises' del objeto part3 al componente Part */}
-      <Part name={props.part3.name} exercises={props.part3.exercises} />
+      {/* Accede al primer objeto del array (índice 0) y pasa sus propiedades a Part */}
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      {/* Accede al segundo objeto del array (índice 1) y pasa sus propiedades a Part */}
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      {/* Accede al tercer objeto del array (índice 2) y pasa sus propiedades a Part */}
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   );
 };
 
+// Componente Total: Muestra el número total de ejercicios.
+// Refactorizado: Ahora recibe el array completo 'parts' como una prop.
 const Total = (props) => {
   //console.log(props)
   return (
     <div>
-      <p>Number of exercises {
-        props.exercises1 + 
-        props.exercises2 + 
-        props.exercises3
-      }
+      <p>
+        Number of exercises{' '}
+        {/* Accede a la propiedad 'exercises' de cada objeto en el array y suma */}
+        {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
       </p>
     </div>
   )
 }
 
+
+// Componente App: Contiene toda la data y renderiza los componentes principales.
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  // Data organizada en un array de objetos
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   
   return (
     <div>
       <Header course={course}/>
-      {/* Refactorizado: Pasa los objetos completos de las partes a Content */}
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
-      <Total 
-        exercises1={part1.exercises} 
-        exercises2={part2.exercises} 
-        exercises3={part3.exercises}
-      />
+      {/* Pasa el array 'parts' completo a Content */}
+      <Content parts={parts} />
+      {/* **Pasa el array 'parts' completo a Total** */}
+      <Total parts={parts} />
     </div>
   )
 }
