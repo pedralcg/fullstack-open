68e035247a7f18722a496ef977dfd116555aa723
Pedro Alcoba <pedralcg@gmail.com>
2025-05-20 21:06:08 +0200

    Part02b - Ejer 2.6 Phonebook, paso 1

---
+++ part02/phonebook/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+
+const App = () => {
+  // Estado para almacenar la lista de personas en la agenda
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+
+  // Estado para controlar el valor del campo de entrada del nuevo nombre
+  const [newName, setNewName] = useState('')
+
+  // Función manejadora para el evento 'submit' del formulario
+  const addPerson = (event) => {
+    event.preventDefault(); // Previene el comportamiento por defecto de enviar el formulario (recargar la página)
+    // Crea un nuevo objeto de persona con el nombre actual del campo de entrada
+    const personObject = {
+      name: newName,
+    };
+    // Actualiza el estado 'persons' añadiendo la nueva persona.
+    //! Importante: Crea una nueva copia del array para mantener la inmutabilidad del estado.
+    setPersons([...persons, personObject]);
+    // Limpia el campo de entrada después de añadir la persona
+    setNewName('');
+  };
+
+  // Función manejadora para el evento 'change' del campo de entrada del nombre
+  const handleNameChange = (event) => {
+    // Actualiza el estado 'newName' con el valor actual del campo de entrada
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      {/* Formulario para añadir nuevas personas */}
+      {/* Asocia la función addPerson al evento submit */}
+      <form onSubmit={addPerson}>
+        <div>
+          name:
+          <input
+            value={newName}
+            // El valor del input está controlado por el estado newName
+            onChange={handleNameChange}
+            // Cada cambio en el input actualiza el estado newName
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+          {/* Botón para enviar el formulario */}
+        </div>
+      </form>
+      {/* Elemento de depuración para ver el valor de newName */}
+      <div>debug: {newName}</div>
+      <h2>Numbers</h2>
+      <div>
+        {/* Muestra la lista de personas */}
+        {persons.map(person => (
+          // Utiliza el nombre de la persona como 'key'
+          <p key={person.name}>{person.name}</p>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
